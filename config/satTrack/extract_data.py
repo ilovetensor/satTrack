76cca5d0e70968142013c36c35bf1059ea44ff94
ilovetensor <rohanshar11@gmail.com>
2023-07-25 13:13:26 +0530

    added live data

--- config/satTrack/extract_data.py
+++ config/satTrack/extract_data.py
@@ -2,6 +2,11 @@ from sgp4.api import Satrec
 from sgp4.api import jday 
 import pandas as pd 
 import numpy as np
+from skyfield.api import EarthSatellite, Loader
+from skyfield.api import load, wgs84
+import matplotlib.pyplot as plt 
+import math
+
 
 def cal_semi_major_axis(n):
     mu = float('3.986004418e+14')
@@ -9,7 +14,6 @@ def cal_semi_major_axis(n):
     deno = ((2*n*np.pi)/(86400))**(2/3)
     return round((num/deno)/1000, 3)
     
-
 def line1_data(line1, save_dict):
     save_dict['launch_year'] = line1[9: 11]
     save_dict['first_derivative_mean_motion'] = line1[33: 43]
@@ -19,6 +23,7 @@ def line1_data(line1, save_dict):
 def line2_data(line2, save_dict):
     save_dict['inclination'] = line2[8: 16]
     save_dict['RAAN'] = line2[17: 25]
+    print(save_dict['RAAN'])
     save_dict['longitude_of_ascending_node'] = round((float(save_dict['RAAN']) * np.pi)/180, 2)
     save_dict['eccentricity'] = '0.'+line2[26: 33]
     save_dict['argument of perigee'] = line2[34: 41]
@@ -28,9 +33,6 @@ def line2_data(line2, save_dict):
     save_dict['semi_major_axis']= cal_semi_major_axis(float(save_dict['mean_motion']))
     save_dict['period'] = round(1440/(float(save_dict['mean_motion'])), 1)
 
-  
-
-
 def convert(TLE_FILE = "tle.txt"):
     save_dict = {}
     TLEs = open(TLE_FILE, 'r')
@@ -38,9 +40,6 @@ def convert(TLE_FILE = "tle.txt"):
     L_1 = []
     L_2 = []
     i = 1
-    
-    
-    
     for line in TLEs:
         j = i
         if i == 1:
@@ -58,15 +57,10 @@ def convert(TLE_FILE = "tle.txt"):
             line2_data(line, save_dict[name])
             j = 1
         i = j
-
-
     dataframe = pd.DataFrame(columns = ['Satellite_name', 'Line_1', 'Line_2', 'Position_vector', 'Speed_vector']) 
     dataframe.Satellite_name = L_Name
     dataframe.Line_1 = L_1
     dataframe.Line_2 = L_2
-    
-
-
     jd, fr = jday(2021, 2, 4, 18, 5, 0)
     L_PosVector = []
     L_SpeedVector = []
@@ -83,3 +77,30 @@ def convert(TLE_FILE = "tle.txt"):
     dataframe.Speed_vector = L_SpeedVector
 
     return dataframe, save_dict
+
+# loading satellite parameters using skyfield
+
+
+def get_live_data(TLE):
+    load = Loader('~/Documents/fishing/SkyData')
+    data = load('de421.bsp')
+    ts = load.timescale() 
+
+    planets = load('de421.bsp')
+    earth = planets['earth']
+
+    ts = load.timescale()
+    minutes = np.arange(0, 240, 2)
+    
+
+    L1, L2 = TLE.splitlines()
+    CARTO_Geo = EarthSatellite(L1, L2)
+    print(CARTO_Geo.epoch.utc_jpl())
+    time = ts.now()
+    geocentric = CARTO_Geo.at(time)
+    lat, lon = wgs84.latlon_of(geocentric)
+    h = wgs84.height_of(geocentric)
+    time_str = f"{time.utc.hour}: {time.utc.minute}: {int(time.utc.second)}"
+    position = {'lat': round(lat.degrees, 2), 'lon': round(lon.degrees, 2), 'height': round(h.km, 2), 'time': time_str}
+    
+    return position
--- config/satTrack/views.py
+++ config/satTrack/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 from sgp4.api import Satrec 
 from sgp4.api import jday 
 import pandas as pd 
-from .extract_data import convert
+from .extract_data import convert, get_live_data
 
 def index(request):
     return render(request, 'home.html',)
 
 def data(request):
     df, save_dict = convert('templates/tle.txt')
-  
-    # save_dict = save_dict.to_html()
-    context = {'data': save_dict}
-    
-  
-    return render(request, 'data.html', context)
+    TLE = """1 44804U 19081A   23196.35840878  .00005378  00000+0  25694-3 0  9994
+2 44804  97.3450 253.5493 0009838 278.3489  81.6631 15.19397374201298"""
+
+        # request.is_ajax() is deprecated since django 3.1
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    if is_ajax:
+        position = get_live_data(TLE)
+        if request.method == 'GET':
+            return JsonResponse({'context': position})
+        return JsonResponse({'status': 'Invalid request'}, status=400)
+        
+    else:
+        context = {'data': save_dict}
+        return render(request, 'data.html', context)
