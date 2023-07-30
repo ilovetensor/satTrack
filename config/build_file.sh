1b1c6cac2bc78d7f09411c192c552146a204e0c8
ilovetensor <rohanshar11@gmail.com>
2023-07-31 00:06:20 +0530

    err21

---
+++ config/build_file.sh
@@ -0,0 +1,8 @@
+echo "BUILD START"
+pip install --upgrade
+pip install python3-dev
+apt install libpq-dev libpq-dev
+pip3 install -r requirements.txt
+python3.9 manage.py collectstatic
+
+echo "BUILD END"
