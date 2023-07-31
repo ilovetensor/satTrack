b71345f80d5d6b511cef6193df1b5f9e50f88f00
ilovetensor <rohanshar11@gmail.com>
2023-07-31 17:40:35 +0530

    err27

--- config/build.sh
+++ config/build.sh
@@ -1,7 +1,6 @@
 echo "BUILD START"
 pip install --upgrade pip
 pip3 install -r requirements.txt
-python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
 echo "BUILD END"
