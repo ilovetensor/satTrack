bdc5dc44b7ddcf90f219e08a271b796633d8cc46
ilovetensor <rohanshar11@gmail.com>
2023-07-31 21:45:12 +0530

    superuser

--- build_file.sh
+++
@@ -1,8 +0,0 @@
-echo "BUILD START"
-pip install --upgrade
-pip install python3-dev
-sudo apt install libpq-dev libpq-dev
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic
-
-echo "BUILD END"
--- config/build.sh
+++ config/build.sh
@@ -1,6 +1,8 @@
 echo "BUILD START"
 pip install --upgrade pip
 pip3 install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate 
+python manage.py createsuperuser --no-input
 python manage.py collectstatic
 echo "BUILD END"
